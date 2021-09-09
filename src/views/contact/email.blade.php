574ce81eabdf515c6534f2a104e528e7997a4d0a
CodersHa <coders.ha2021@gmail.com>
2021-09-09 15:28:30 +0200

    first commit

---
+++ src/views/contact/email.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Introduction
+from {{ $name }}
+{{ $message }}
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
